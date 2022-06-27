de35cdf394da01a25ad3fe5fdb76c788541645ea
ArtemPro123 <protasavytsky@gmail.com>
2022-06-27 20:32:03 +0100

    adding tags

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -5,7 +5,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"}, monochrome=true, plugin = {"pretty", "html:target/HtmlReports.html"})
+@CucumberOptions(features="src/test/resources/Features", 
+glue= {"StepDefinitions"},
+monochrome=true, 
+plugin = {"pretty", "html:target/HtmlReports.html"},
+tags="@smokeTest")
 public class TestRunner {
 
 	
--- src/test/resources/Features/login.feature
+++ src/test/resources/Features/login.feature
@@ -1,7 +1,9 @@
 #Author: artempro123
 
+@smokeFeature
 Feature: feature to test login functionality
 
+@smokeTest
   Scenario: check login is successful with valid credentials
     Given user is on login page
     When user enters username and password
